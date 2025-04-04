7c0dc56ba3c968b4b56a97eb943f67abf5737905
DrinuVella <andreVella2111@gmail.com>
2025-04-04 10:22:26 +0200

    Library Project fix

---
+++ AndreVellaEPProject/Controllers/PollController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class PollController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
